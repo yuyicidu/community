339185680792dc5fe17f80fff7bea7f1834c5a8a
yuyicidu <597799047@qq.com>
2019-11-05 22:51:58 +0800

    test

--- src/main/java/com/example/community/CommunityApplication.java
+++ src/main/java/com/example/community/CommunityApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CommunityApplication {
 
     public static void main(String[] args) {
+        System.out.println("11111");
         SpringApplication.run(CommunityApplication.class, args);
     }
 
